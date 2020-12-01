d720a08f5f56c39a4705b01cb2ee1f9864924ada
hadiazb <hadiazb@unal.edu.co>
2020-12-01 14:42:57 -0500

    [ADD] service cape

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,36 @@
-console.log('Hola mundo!!!');
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import Config from './config/index';
+
+export default class Server {
+  private port: number = Number(Config.api().port);
+
+  private app!: express.Application;
+
+  public constructor() {
+    this.createApp();
+    this.configApp();
+    this.runServer();
+  }
+
+  public getApp(): express.Application {
+    return this.app;
+  }
+
+  private createApp(): void {
+    this.app = express();
+  }
+
+  private configApp(): void {
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use(helmet());
+  }
+
+  private runServer(): void {
+    this.app.listen(this.port, () =>
+      console.log('Running in the port %s', this.port)
+    );
+  }
+}
